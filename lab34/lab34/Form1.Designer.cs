b32d7c5d3b59e7bd4a651f78f228c9498770714f
AXELL2022 <yasunyatskiy1@gmail.com>
2025-12-06 14:17:30 +0200

    Lab34

---
+++ lab34/lab34/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace StudentApp
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.NameColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.YearColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.RatingColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+
+            // label1
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ім\'я студента:";
+
+            // textBox1
+            this.textBox1.Location = new System.Drawing.Point(180, 20);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(200, 20);
+            this.textBox1.TabIndex = 1;
+
+            // label2
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Рік вступу:";
+
+            // textBox2
+            this.textBox2.Location = new System.Drawing.Point(180, 60);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(200, 20);
+            this.textBox2.TabIndex = 3;
+
+            // label3
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Рейтинг:";
+
+            // textBox3
+            this.textBox3.Location = new System.Drawing.Point(180, 100);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(200, 20);
+            this.textBox3.TabIndex = 5;
+
+            // button1
+            this.button1.Location = new System.Drawing.Point(20, 150);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 30);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Додати студента";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+
+            // button2
+            this.button2.Location = new System.Drawing.Point(180, 150);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(150, 30);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Запам\'ятати у файл";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+
+            // button3
+            this.button3.Location = new System.Drawing.Point(340, 150);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(150, 30);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Середній рейтинг";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+
+            // button4
+            this.button4.Location = new System.Drawing.Point(500, 150);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(150, 30);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Закрити";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+
+            // buttonAbout
+            this.buttonAbout = new System.Windows.Forms.Button();
+            this.buttonAbout.Location = new System.Drawing.Point(660, 150);
+            this.buttonAbout.Name = "buttonAbout";
+            this.buttonAbout.Size = new System.Drawing.Size(40, 30);
+            this.buttonAbout.TabIndex = 10;
+            this.buttonAbout.Text = "?";
+            this.buttonAbout.UseVisualStyleBackColor = true;
+            this.buttonAbout.Click += new System.EventHandler(this.buttonAbout_Click);
+
+            // label4
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(20, 200);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(150, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Список студентів:";
+
+            // dataGridView1
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.NameColumn,
+            this.YearColumn,
+            this.RatingColumn});
+            this.dataGridView1.Location = new System.Drawing.Point(20, 230);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(680, 200);
+            this.dataGridView1.TabIndex = 11;
+
+            // NameColumn
+            this.NameColumn.HeaderText = "Ім\'я";
+            this.NameColumn.Name = "NameColumn";
+            this.NameColumn.Width = 200;
+
+            // YearColumn
+            this.YearColumn.HeaderText = "Рік вступу";
+            this.YearColumn.Name = "YearColumn";
+            this.YearColumn.Width = 200;
+
+            // RatingColumn
+            this.RatingColumn.HeaderText = "Рейтинг";
+            this.RatingColumn.Name = "RatingColumn";
+            this.RatingColumn.Width = 200;
+
+            // label5
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(20, 450);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(150, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Середній рейтинг:";
+
+            // textBox4
+            this.textBox4.Location = new System.Drawing.Point(180, 450);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(200, 20);
+            this.textBox4.TabIndex = 13;
+
+            // Form1
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(750, 500);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.buttonAbout);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Керування студентами";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Button buttonAbout;
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NameColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn YearColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn RatingColumn;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
---
+++ lab34/lab34/Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace StudentApp
+{
+    class Student
+    {
+        string name;
+        int year;
+        double rating;
+
+        public Student()
+        {
+            name = "New";
+            year = 2018;
+            rating = 90.5;
+        }
+
+        public Student(string n, int y, double r)
+        {
+            name = n;
+            year = y;
+            rating = r;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Year
+        {
+            get { return year; }
+            set { year = value; }
+        }
+
+        public double Rating
+        {
+            get { return rating; }
+            set { rating = value; }
+        }
+    }
+
+    public partial class Form1 : Form
+    {
+        private List<Student> students = new List<Student>();
+        private string authorSurname = "Ясиняцький";
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadStudentsFromFile();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Student s = new Student(
+                    textBox1.Text,
+                    int.Parse(textBox2.Text),
+                    double.Parse(textBox3.Text.Replace(",", ".")));
+
+                students.Add(s);
+                dataGridView1.Rows.Add(s.Name, s.Year, s.Rating);
+
+                // Оновлюємо заголовок вікна з прізвищем останнього студента
+                if (students.Count > 0)
+                {
+                    this.Text = "Керування студентами - " + students[students.Count - 1].Name;
+                }
+
+                MessageBox.Show("Студент додан!");
+                textBox1.Text = "Name";
+                textBox2.Text = "2018";
+                textBox3.Text = "90,5";
+            }
+            catch
+            {
+                MessageBox.Show("Помилка! Перевірте введені дані.");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (StreamWriter sw = new StreamWriter("student.txt", false, Encoding.Default))
+                {
+                    foreach (Student s in students)
+                    {
+                        sw.WriteLine(s.Name + " " + s.Year + " " + s.Rating);
+                    }
+                }
+                MessageBox.Show("Дані збережено в файл student.txt!");
+            }
+            catch
+            {
+                MessageBox.Show("Помилка при збереженні!");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (students.Count == 0)
+            {
+                textBox4.Text = "0";
+                return;
+            }
+
+            double sum = 0;
+            foreach (Student s in students)
+            {
+                sum += s.Rating;
+            }
+            textBox4.Text = (sum / students.Count).ToString("F2");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void ShowAboutForm()
+        {
+            Form aboutForm = new Form();
+            aboutForm.Text = "Про автора";
+            aboutForm.Width = 400;
+            aboutForm.Height = 300;
+            aboutForm.StartPosition = FormStartPosition.CenterParent;
+
+            Label label = new Label();
+            label.Text = "Програма для керування студентами\n\n" +
+                        "Автор:" + authorSurname + " К.М.\n" +
+                        "Группа: БІКСб22440д\n" +
+                        "Спеціальність: Кібербезпека\n" +
+                        "Семестр: 3\n\n" +
+                        "Версія: 1.0\n" +
+                        "Рік: 2025\n\n" +
+                        "Лабораторна робота №34";
+            label.Dock = DockStyle.Fill;
+            label.TextAlign = ContentAlignment.MiddleCenter;
+            label.Font = new System.Drawing.Font("Arial", 10);
+
+            Button okButton = new Button();
+            okButton.Text = "OK";
+            okButton.Width = 100;
+            okButton.Height = 30;
+            okButton.Dock = DockStyle.Bottom;
+            okButton.Click += (s, e) => aboutForm.Close();
+
+            aboutForm.Controls.Add(label);
+            aboutForm.Controls.Add(okButton);
+            aboutForm.ShowDialog(this);
+        }
+
+        private void LoadStudentsFromFile()
+        {
+            try
+            {
+                if (!File.Exists("student.txt"))
+                    return;
+
+                using (StreamReader sr = new StreamReader("student.txt", Encoding.Default))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        string[] parts = line.Split(' ');
+                        if (parts.Length >= 3)
+                        {
+                            Student s = new Student(
+                                parts[0],
+                                int.Parse(parts[1]),
+                                double.Parse(parts[2].Replace(",", ".")));
+                            students.Add(s);
+                            dataGridView1.Rows.Add(s.Name, s.Year, s.Rating);
+                        }
+                    }
+                }
+
+                // Оновлюємо заголовок з прізвищем першого завантаженого студента
+                if (students.Count > 0)
+                {
+                    this.Text = "Керування студентами -  " + students[0].Name;
+                }
+            }
+            catch { }
+        }
+
+        private void buttonAbout_Click(object sender, EventArgs e)
+        {
+            ShowAboutForm();
+        }
+    }
+}
